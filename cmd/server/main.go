c658a1050ff2aa3ad796b1de05b8e42bc541bd11
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-02-22 12:43:09 +0600

    feat: add main file for the project

---
+++ cmd/server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"obyoy-backend/bootstrap/server"
+	"obyoy-backend/config"
+	"obyoy-backend/container"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+
+	if err := server.Viper(); err != nil {
+		logrus.Fatal(err)
+	}
+
+	c := container.New()
+	server.Logrus()
+	//Cfg provides configuration related providers to the container
+	server.Cfg(c)
+
+	server.Handler(c)
+	server.Route(c)
+	server.Store(c)
+	server.Mongo(c)
+	server.MongoCollections(c)
+	server.Redis(c)
+	server.Cache(c)
+	server.Middleware(c)
+	server.User(c)
+	server.WS(c)
+	server.Validator(c)
+	server.Event(c)
+
+	c.Resolve(
+		func(cfg config.Server, handler http.Handler) {
+			logrus.Info("Starting server at port ", cfg.Port)
+			http.ListenAndServe(":"+cfg.Port, handler)
+		},
+	)
+}
