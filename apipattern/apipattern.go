a4dd8ac7213552093b531a3164b54d5d397aa892
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-02-22 09:34:34 +0600

    feat: add file for storing all the apipatterns of the project

---
+++ apipattern/apipattern.go
@@ -0,0 +1,14 @@
+// UserList holds the api for giving user list
+const UserList string = "/api/v1/alluser"
+
+// LoginUser holds the api for logging user
+const LoginUser string = "/api/v1/users/login"
+
+// RegistrationToken holds holds the api for giving registration token
+const RegistrationToken string = "/api/v1/registration/token"
+
+// UserRegistration holds the api for registering a user
+const UserRegistration string = "/api/v1/users/registration"
+
+// UserSearch holds the api for searching user
+const UserSearch string = "/api/v1/users/search"
