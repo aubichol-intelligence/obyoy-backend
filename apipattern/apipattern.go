69956d7ad14ec9f084c89377d38518261e72d4f1
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-12 20:30:17 +0600

    chore: add api patterns for datastream and dataset

--- apipattern/apipattern.go
+++ apipattern/apipattern.go
@@ -12,3 +12,27 @@ const UserRegistration string = "/api/v1/users/registration"
 
 // UserSearch holds the api for searching user
 const UserSearch string = "/api/v1/users/search"
+
+// DatasetCreate holds the api string for creating a dataset
+const DatasetCreate string = "/api/v1/dataset/create"
+
+// DatasetRead holds the api string for reading datasets
+const DatasetRead string = "/api/v1/dataset/get/{id}"
+
+// DatasetUpdate holds the api string for updating dataset
+const DatasetUpdate string = "/api/v1/dataset/update"
+
+// DatasetDelete holds the api string for getting a dataset
+const DatasetDelete string = "/api/v1/dataset/delete"
+
+// DatasetCreate holds the api string for creating a dataset
+const DatastreamCreate string = "/api/v1/datastream/create"
+
+// DatasetRead holds the api string for reading datasets
+const DatastreamRead string = "/api/v1/datastream/get/{id}"
+
+// DatasetUpdate holds the api string for updating dataset
+const DatastreamUpdate string = "/api/v1/datastream/update"
+
+// DatasetDelete holds the api string for getting a dataset
+const DatastreamDelete string = "/api/v1/datastream/delete"
