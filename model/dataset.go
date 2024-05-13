ae210fa691ece0123144f5bb03578a4f97c25edb
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-13 20:40:21 +0600

    chore: update types for dataset

--- model/dataset.go
+++ model/dataset.go
@@ -7,10 +7,13 @@ import (
 
 // Translation defines user's translation
 type Dataset struct {
+	ID        string    `json:"id"`
 	Key       string    `json:"key"`
 	UserID    string    `json:"user_id"`
 	ExpiredAt time.Time `json:"expire_at"`
 	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	IsDeleted bool      `json:"is_deleted"`
 }
 
 func (s *Dataset) ToByte() ([]byte, error) {
