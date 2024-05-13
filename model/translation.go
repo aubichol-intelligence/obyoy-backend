c5d64f4eb43bbdb03b9d267983753cd8610f1d7a
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-13 17:26:13 +0600

    chore: add proper data type in the receiver

--- model/translation.go
+++ model/translation.go
@@ -13,11 +13,12 @@ type Translation struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
-func (s *Session) ToByte() ([]byte, error) {
+func (s *Translation) ToByte() ([]byte, error) {
 	return json.Marshal(s)
 }
-	//Username  string
 
-func (s *Session) FromBytes(bytes []byte) error {
+//Username  string
+
+func (s *Translation) FromBytes(bytes []byte) error {
 	return json.Unmarshal(bytes, s)
 }
