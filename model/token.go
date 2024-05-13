0e2af0cb8b07e58317afe1db7f3477d70ed23543
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-13 17:16:06 +0600

    chore: fix type in the receiver and remove redundant code

--- model/token.go
+++ model/token.go
@@ -5,30 +5,6 @@ import (
 	"time"
 )
 
-// Session defines user's session
-type Session struct {
-	Key       string    `json:"key"`
-	UserID    string    `json:"user_id"`
-	ExpiredAt time.Time `json:"expire_at"`
-	CreatedAt time.Time `json:"created_at"`
-}
-
-func (s *Session) ToByte() ([]byte, error) {
-	return json.Marshal(s)
-}
-	//Username  string
-
-func (s *Session) FromBytes(bytes []byte) error {
-	return json.Unmarshal(bytes, s)
-}
-(base) nelson@NELSONs-MacBook-Pro model % ls
-delivery.go		maps.go			order.go		session.go		state.go		token.go
-email.go		menuitem.go		restaurant.go		sms.go			staticcontent.go	user.go
-(base) nelson@NELSONs-MacBook-Pro model % cat token.go
-package model
-
-import "time"
-
 // Token defines token model
 type Token struct {
 	ID        string
@@ -36,4 +12,14 @@ type Token struct {
 	Token     string
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func (s *Token) ToByte() ([]byte, error) {
+	return json.Marshal(s)
+}
+
+//Username  string
+
+func (s *Token) FromBytes(bytes []byte) error {
+	return json.Unmarshal(bytes, s)
 }
