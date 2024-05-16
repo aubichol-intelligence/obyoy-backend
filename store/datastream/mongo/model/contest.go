885bee92d2c9f24235b5f89d8593bfd6db867682
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-16 20:01:38 +0600

    chore: add attributes  and assignments for datastream creation

--- store/datastream/mongo/model/contest.go
+++ store/datastream/mongo/model/contest.go
@@ -10,28 +10,16 @@ import (
 
 // Datastream holds db data type for deliveries
 type Datastream struct {
-	ID                primitive.ObjectID `bson:"_id,omitempty"`
-	Name              string             `bson:"name,omitempty"`
-	Phone             string             `bson:"phone_number,omitempty"`
-	Address           string             `bson:"address,omitempty"`
-	UserID            primitive.ObjectID `bson:"user_id,omitempty"`
-	DriverID          primitive.ObjectID `bson:"driver_id,omitempty"`
-	OrderID           primitive.ObjectID `bson:"order_id,omitempty"`
-	RestaurantID      primitive.ObjectID `bson:"restaurant_id,omitempty"`
-	RestaurantName    string             `bson:"restaurant_name,omitempty"`
-	RestaurantAddress string             `bson:"restaurant_address,omitempty"`
-	RestaurantPhone   string             `bson:"restaurant_phone,omitempty"`
-	CustomerDetails   Credentials        `bson:"customer_details,omitempty"`
-	RestaurantDetails Credentials        `bson:"restaurant_details,omitempty"`
-	Note              string             `bson:"note,omitempty,omitempty"`
-	Amount            float64            `bson:"amount,omitempty"`
-	IsActive          bool               `bson:"is_active,omitempty"`
-	Distance          string             `bson:"distance,omitempty"`
-	State             string             `bson:"state,omitempty"`
-	ItemCount         int                `bson:"item_count,omitempty"`
-	CreatedAt         time.Time          `bson:"created_at,omitempty"`
-	UpdatedAt         time.Time          `bson:"updated_at,omitempty"`
-	IsDeleted         bool               `bson:"is_deleted,omitempty"`
+	ID              primitive.ObjectID `bson:"_id,omitempty"`
+	Name            string             `bson:"name,omitempty"`
+	SourceSentence  string             `bson:"source_sentence"`
+	LineNumber      int32              `bson:"line_number"`
+	DatasetID       primitive.ObjectID `bson:"dataset_id"`
+	TimesTranslated int32              `bson:"times_translated,omitempty"`
+	TimesReviewed   int32              `bson:"times_reviewed,omitempty"`
+	CreatedAt       time.Time          `bson:"created_at,omitempty"`
+	UpdatedAt       time.Time          `bson:"updated_at,omitempty"`
+	IsDeleted       bool               `bson:"is_deleted,omitempty"`
 }
 
 type Credentials struct {
@@ -46,6 +34,10 @@ type Credentials struct {
 func (d *Datastream) FromModel(modelDelivery *model.Datastream) error {
 	d.CreatedAt = modelDelivery.CreatedAt
 	d.UpdatedAt = modelDelivery.UpdatedAt
+	d.SourceSentence = modelDelivery.SourceSentence
+	d.LineNumber = modelDelivery.LineNumber
+	d.TimesTranslated = modelDelivery.TimesTranslated
+	d.TimesReviewed = modelDelivery.TimesReviewed
 
 	var err error
 
@@ -53,6 +45,14 @@ func (d *Datastream) FromModel(modelDelivery *model.Datastream) error {
 		d.ID, err = primitive.ObjectIDFromHex(modelDelivery.ID)
 	} else {
 		d.ID = primitive.NewObjectID()
+	}
+
+	if err != nil {
+		return err
+	}
+
+	if modelDelivery.DatasetID != "" {
+		d.DatasetID, err = primitive.ObjectIDFromHex(modelDelivery.DatasetID)
 	}
 
 	if err != nil {
