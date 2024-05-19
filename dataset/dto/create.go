be93b6264a353a25292f7e60db805863614f594f
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-19 22:09:02 +0600

    chore: update attribute type for creating dataset

--- dataset/dto/create.go
+++ dataset/dto/create.go
@@ -12,15 +12,15 @@ import (
 
 // contest provides dto for contest request
 type Create struct {
-	ID              string `json:"contest_id"`
-	Set             string `json:"set"`
-	Name            string `json:"name"`
-	TotalLines      int32  `json:"total_lines"`
-	SourceLanguage  string `json:"source_language"`
-	UploaderID      string `json:"uploader_id"`
-	TranslatedLines int32  `json:"translated_lines"`
-	ReviewedLines   int32  `json:"reviewed_lines"`
-	IsDeleted       bool   `json:"is_deleted"`
+	ID              string   `json:"contest_id"`
+	Set             []string `json:"set"`
+	Name            string   `json:"name"`
+	TotalLines      int32    `json:"total_lines"`
+	SourceLanguage  string   `json:"source_language"`
+	UploaderID      string   `json:"uploader_id"`
+	TranslatedLines int32    `json:"translated_lines"`
+	ReviewedLines   int32    `json:"reviewed_lines"`
+	IsDeleted       bool     `json:"is_deleted"`
 }
 
 // Validate validates contest request data
