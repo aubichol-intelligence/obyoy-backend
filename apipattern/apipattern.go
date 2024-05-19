114bea9bda4cc9db8cbc89a33b421a0ebd94aae0
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-19 16:38:48 +0600

    chore: update comments and remove redundant api paths

--- apipattern/apipattern.go
+++ apipattern/apipattern.go
@@ -30,41 +30,29 @@ const DatasetUpdate string = "/api/v1/dataset/update"
 // DatasetDelete holds the api string for getting a dataset
 const DatasetDelete string = "/api/v1/dataset/delete"
 
-// DatasetCreate holds the api string for creating a dataset
+// DatasetCreate holds the api string for creating a datastream
 const DatastreamCreate string = "/api/v1/datastream/create"
 
-// DatasetRead holds the api string for reading datasets
+// DatasetRead holds the api string for reading datastreams
 const DatastreamRead string = "/api/v1/datastream/get"
 
-// DatasetRead holds the api string for reading datasets
+// DatasetRead holds the api string for reading datastreams
 const DatastreamReadNext string = "/api/v1/datastream/getnext"
 
-// DatasetUpdate holds the api string for updating dataset
+// DatasetUpdate holds the api string for updating datastream
 const DatastreamUpdate string = "/api/v1/datastream/update"
 
 // DatasetDelete holds the api string for getting a dataset
 const DatastreamDelete string = "/api/v1/datastream/delete"
 
-// DatasetCreate holds the api string for creating a dataset
+// DatasetCreate holds the api string for creating a parallelsentence
 const ParallelsentenceCreate string = "/api/v1/parallelsentence/create"
 
-// DatasetRead holds the api string for reading datasets
+// DatasetRead holds the api string for reading parallelsentences
 const ParallelsentenceRead string = "/api/v1/parallelsentence/get/{id}"
 
-// DatasetUpdate holds the api string for updating dataset
+// DatasetUpdate holds the api string for updating parallelsentence
 const ParallelsentenceUpdate string = "/api/v1/parallelsentence/update"
 
-// DatasetDelete holds the api string for getting a dataset
+// DatasetDelete holds the api string for getting a parallelsentence
 const ParallelsentenceDelete string = "/api/v1/parallelsentence/delete"
-
-// DatasetCreate holds the api string for creating a dataset
-const TranslationCreate string = "/api/v1/translation/create"
-
-// DatasetRead holds the api string for reading datasets
-const TranslationRead string = "/api/v1/translation/get/{id}"
-
-// DatasetUpdate holds the api string for updating dataset
-const TranslationUpdate string = "/api/v1/translation/update"
-
-// DatasetDelete holds the api string for getting a dataset
-const TranslationDelete string = "/api/v1/translation/delete"
