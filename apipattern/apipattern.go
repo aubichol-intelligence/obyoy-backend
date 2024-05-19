5ce0c82a424c7e716743ae9b31a8136903609cdf
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-19 16:42:21 +0600

    chore: return translation path in case necessary

--- apipattern/apipattern.go
+++ apipattern/apipattern.go
@@ -56,3 +56,15 @@ const ParallelsentenceUpdate string = "/api/v1/parallelsentence/update"
 
 // DatasetDelete holds the api string for getting a parallelsentence
 const ParallelsentenceDelete string = "/api/v1/parallelsentence/delete"
+
+// DatasetCreate holds the api string for creating a dataset
+const TranslationCreate string = "/api/v1/translation/create"
+
+// DatasetRead holds the api string for reading datasets
+const TranslationRead string = "/api/v1/translation/get/{id}"
+
+// DatasetUpdate holds the api string for updating dataset
+const TranslationUpdate string = "/api/v1/translation/update"
+
+// DatasetDelete holds the api string for getting a dataset
+const TranslationDelete string = "/api/v1/translation/delete"
