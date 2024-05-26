068e1f181e3327a9a3d3d3b067b7ebc45f875f42
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-26 21:35:02 +0600

    feat: add data type for listing the dataset

--- dataset/dto/list.go
+++ dataset/dto/list.go
@@ -1,7 +1,44 @@
 package dto
 
-//ReadReq stores order read request data
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"obyoy-backend/errors"
+
+	validator "gopkg.in/go-playground/validator.v9"
+)
+
+// ReadReq stores order read request data
 type ListReq struct {
-	UserID string
-	Contest  string
+	UserID  string
+	Contest string
+	Skip    int64 `json:"skip"`
+	Limit   int64 `json:"limit"`
+}
+
+// Validate validates restaurant data
+func (d *ListReq) Validate(validate *validator.Validate) error {
+	if err := validate.Struct(d); err != nil {
+		return fmt.Errorf(
+			"%s:%w",
+			err.Error(),
+			&errors.Invalid{
+				errors.Base{"invalid data for user", false},
+			},
+		)
+	}
+	return nil
+}
+
+// FromReader reads restaurant from request body
+func (d *ListReq) FromReader(reader io.Reader) error {
+	err := json.NewDecoder(reader).Decode(d)
+	if err != nil {
+		return fmt.Errorf("%s:%w", err.Error(), &errors.Invalid{
+			Base: errors.Base{"invalid user list read data", false},
+		})
+	}
+
+	return nil
 }
