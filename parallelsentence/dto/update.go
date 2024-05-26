f3deb3b9fcd7f3d2f43fbf8f4d1618739cfc0141
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-27 03:47:28 +0600

    feat: add proper attributes for parallel sentence update

--- parallelsentence/dto/update.go
+++ parallelsentence/dto/update.go
@@ -12,19 +12,18 @@ import (
 
 // Update provides dto for contest update
 type Update struct {
-	ID                string  `json:"contest_id"`
-	Name              string  `json:"name"`
-	Phone             string  `json:"phone_number"`
-	Address           string  `json:"address"`
-	UserID            string  `json:"user_id"`
-	Duration          int     `json:"duration"`
-	RestaurantAddress string  `json:"restaurant_address"`
-	RestaurantName    string  `json:"restaurant_name"`
-	RestaurantPhone   string  `json:"restaurant_phone"`
-	Note              string  `json:"note"`
-	Amount            float64 `json:"amount"`
-	ItemCount         int     `json:"item_count"`
-	State             string  `json:"state"`
+	ID                  string   `json:"contest_id"`
+	SourceSentence      string   `json:"source_sentence"`
+	DatasetID           string   `json:"dataset_id"`
+	DatastreamID        string   `json:"datastream_id"`
+	SourceLanguage      string   `json:"source_language"`
+	DestinationSentence string   `json:"destination_sentence"`
+	DestinationLanguage string   `json:"destination_language"`
+	TimesReviewed       int      `json:"times_reviewed"`
+	TranslatorID        string   `json:"translator_id"`
+	Reviewers           []string `json:"reviewers"`
+	ReviewedLines       []string `json:"reviewed_lines"`
+	IsDeleted           bool     `json:"is_deleted"`
 }
 
 // Validate validates contest update data
