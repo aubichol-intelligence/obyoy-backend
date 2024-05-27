76ef1c2e2fd1146f435e9588953938a34234cda5
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-28 02:13:37 +0600

    feat: add attributes for translation

--- model/translation.go
+++ model/translation.go
@@ -7,13 +7,22 @@ import (
 
 // Translation defines user's translation
 type Translation struct {
-	ID        string    `json:translation_id"`
-	Key       string    `json:"key"`
-	UserID    string    `json:"user_id"`
-	ExpiredAt time.Time `json:"expire_at"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	IsDeleted bool      `json:"is_deleted"`
+	ID                  string    `json:"translation_id"`
+	UserID              string    `json:"user_id"`
+	Name                string    `json:"name"`
+	SourceLanguage      string    `json:"source_language"`
+	SourceSentence      string    `json:"source_sentence"`
+	DestinationLanguage string    `json:"destination_language"`
+	DestinationSentence string    `json:"destination_sentence"`
+	DatasetID           string    `json:"dataset_id"`
+	DatastreamID        string    `json:"datastream_id"`
+	LineNumber          int       `json:"line_number"`
+	TranslatorID        string    `json:"translator_id"`
+	ReviewerID          string    `json:"reviewer_id"`
+	ExpiredAt           time.Time `json:"expire_at"`
+	CreatedAt           time.Time `json:"created_at"`
+	UpdatedAt           time.Time `json:"updated_at"`
+	IsDeleted           bool      `json:"is_deleted"`
 }
 
 func (s *Translation) ToByte() ([]byte, error) {
