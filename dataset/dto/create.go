ba4cd749cfd41deae8e282d152874518a306fb12
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-27 07:47:19 +0600

    chore: update comments and error messages

--- dataset/dto/create.go
+++ dataset/dto/create.go
@@ -10,7 +10,7 @@ import (
 	validator "gopkg.in/go-playground/validator.v9"
 )
 
-// contest provides dto for contest request
+// dataset provides dto for dataset request
 type Create struct {
 	ID              string   `json:"dataset_id"`
 	Set             []string `json:"set"`
@@ -23,26 +23,26 @@ type Create struct {
 	IsDeleted       bool     `json:"is_deleted"`
 }
 
-// Validate validates contest request data
+// Validate validates dataset request data
 func (d *Create) Validate(validate *validator.Validate) error {
 	if err := validate.Struct(d); err != nil {
 		return fmt.Errorf(
 			"%s:%w",
 			err.Error(),
 			&errors.Invalid{
-				errors.Base{"invalid data for contest", false},
+				errors.Base{"invalid data for dataset", false},
 			},
 		)
 	}
 	return nil
 }
 
-// FromReader reads contest request from request body
+// FromReader reads dataset request from request body
 func (d *Create) FromReader(reader io.Reader) error {
 	err := json.NewDecoder(reader).Decode(d)
 	if err != nil {
 		return fmt.Errorf("%s:%w", err.Error(), &errors.Invalid{
-			Base: errors.Base{"invalid contest data", false},
+			Base: errors.Base{"invalid dataset data", false},
 		})
 	}
 
