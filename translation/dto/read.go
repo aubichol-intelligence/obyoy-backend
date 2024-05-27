33b1cdf2f360d06d3945ae2cfba7fafdc7db5c94
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-28 02:28:28 +0600

    feat: update assignments for reading the translation data

--- translation/dto/read.go
+++ translation/dto/read.go
@@ -2,22 +2,38 @@ package dto
 
 import "obyoy-backend/model"
 
-// ReadReq stores order read request data
+// ReadReq stores translation read request data
 type ReadReq struct {
 	UserID        string
 	TranslationID string
 }
 
-// ReadReq stores order read request data
+// ReadReq stores translation read request data
 type ReadResp struct {
-	UserID    string
-	ContestID string
+	ID                  string `json:"translation_id"`
+	DatasetID           string `json:"dataset_id"`
+	DatastreamID        string `json:"datastream_id"`
+	TranslatorID        string `json:"translator_id"`
+	ReviewerID          string `json:"reviewer_id"`
+	SourceSentence      string `json:"source_sentence"`
+	SourceLanguage      string `json:"source_language"`
+	DestinationSentence string `json:"destination_sentence"`
+	DestinationLanguage string `json:"destination_language"`
+	LineNumber          int    `json:"line_number"`
+	Name                string `json:"name"`
 }
 
 // FromModel converts the model data to response data
 func (r *ReadResp) FromModel(delivery *model.Translation) {
-	// r.Name = delivery.Name
-	// r.Phone = delivery.Phone
-	// r.Address = delivery.Address
-	// r.UserID = delivery.UserID
+	r.ID = delivery.ID
+	r.DatasetID = delivery.DatasetID
+	r.DatastreamID = delivery.DatastreamID
+	r.TranslatorID = delivery.TranslatorID
+	r.ReviewerID = delivery.ReviewerID
+	r.SourceLanguage = delivery.SourceLanguage
+	r.SourceSentence = delivery.SourceSentence
+	r.DestinationSentence = delivery.DestinationSentence
+	r.DestinationLanguage = delivery.DestinationSentence
+	r.LineNumber = delivery.LineNumber
+	r.Name = delivery.Name
 }
