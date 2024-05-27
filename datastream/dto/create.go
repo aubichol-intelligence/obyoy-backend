7da68ca41dbd3c6d2ffff039e346fe336534f17a
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-27 07:52:35 +0600

    chore: update comments and errors messages

--- datastream/dto/create.go
+++ datastream/dto/create.go
@@ -10,7 +10,7 @@ import (
 	validator "gopkg.in/go-playground/validator.v9"
 )
 
-// contest provides dto for contest request
+// datastream provides dto for datastream request
 type Create struct {
 	ID              string `json:"datastream_id"`
 	SourceSentence  string `json:"source_sentence"`
@@ -23,26 +23,26 @@ type Create struct {
 	IsDeleted       bool   `json:"is_deleted"`
 }
 
-// Validate validates contest request data
+// Validate validates datastream request data
 func (d *Create) Validate(validate *validator.Validate) error {
 	if err := validate.Struct(d); err != nil {
 		return fmt.Errorf(
 			"%s:%w",
 			err.Error(),
 			&errors.Invalid{
-				errors.Base{"invalid data for contest", false},
+				errors.Base{"invalid data for datastream", false},
 			},
 		)
 	}
 	return nil
 }
 
-// FromReader reads contest request from request body
+// FromReader reads datastream request from request body
 func (d *Create) FromReader(reader io.Reader) error {
 	err := json.NewDecoder(reader).Decode(d)
 	if err != nil {
 		return fmt.Errorf("%s:%w", err.Error(), &errors.Invalid{
-			Base: errors.Base{"invalid contest data", false},
+			Base: errors.Base{"invalid datastream data", false},
 		})
 	}
 
