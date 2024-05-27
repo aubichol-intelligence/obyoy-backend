2fcbc47cecb3bbd1ae1efb63238a7fd9aacff3dd
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-27 20:59:58 +0600

    feat: add data type for listing parallel sentence

--- parallelsentence/dto/list.go
+++ parallelsentence/dto/list.go
@@ -1,7 +1,43 @@
 package dto
 
-//ReadReq stores order read request data
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"obyoy-backend/errors"
+
+	validator "gopkg.in/go-playground/validator.v9"
+)
+
+// ReadReq stores order read request data
 type ListReq struct {
 	UserID string
-	Contest  string
+	Skip   int64 `json:"skip"`
+	Limit  int64 `json:"limit"`
+}
+
+// Validate validates user data
+func (d *ListReq) Validate(validate *validator.Validate) error {
+	if err := validate.Struct(d); err != nil {
+		return fmt.Errorf(
+			"%s:%w",
+			err.Error(),
+			&errors.Invalid{
+				errors.Base{"invalid data for user", false},
+			},
+		)
+	}
+	return nil
+}
+
+// FromReader reads user from request body
+func (d *ListReq) FromReader(reader io.Reader) error {
+	err := json.NewDecoder(reader).Decode(d)
+	if err != nil {
+		return fmt.Errorf("%s:%w", err.Error(), &errors.Invalid{
+			Base: errors.Base{"invalid user list read data", false},
+		})
+	}
+
+	return nil
 }
