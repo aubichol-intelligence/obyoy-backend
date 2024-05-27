3a26008675fe150a28dee67d5ebc6e2095343985
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-27 07:00:13 +0600

    feat: add flag for introducing deleting to parallel sentence

--- model/parallelsentence.go
+++ model/parallelsentence.go
@@ -21,6 +21,7 @@ type Parallelsentence struct {
 	ExpiredAt           time.Time `json:"expire_at"`
 	CreatedAt           time.Time `json:"created_at"`
 	UpdatedAt           time.Time `json:"updated_at"`
+	IsDeleted           bool      `json:'false"`
 }
 
 func (s *Parallelsentence) ToByte() ([]byte, error) {
