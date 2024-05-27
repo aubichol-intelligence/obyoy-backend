1fc37709fa5cb6f3b9f3766673da81b546f2e4b4
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-27 21:00:17 +0600

    feat: add controller for listing parallel sentence

---
+++ api/parallelsentence/list.go
@@ -0,0 +1,161 @@
+package parallelsentence
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+
+	"obyoy-backend/api/middleware"
+	"obyoy-backend/api/routeutils"
+	"obyoy-backend/apipattern"
+	"obyoy-backend/errors"
+	"obyoy-backend/parallelsentence"
+	"obyoy-backend/parallelsentence/dto"
+
+	"github.com/go-chi/chi"
+	"github.com/sirupsen/logrus"
+)
+
+// listHandler holds the handler that searches for dataset
+type listHandler struct {
+	lister parallelsentence.Lister
+}
+
+func (ch *listHandler) decodeBody(
+	body io.ReadCloser,
+) (
+	parallelsentence dto.ListReq,
+	err error,
+) {
+	err = parallelsentence.FromReader(body)
+	return
+}
+
+// querySkip skips number of datasets for a request
+func (list *listHandler) querySkip(
+	r *http.Request,
+) (skip int, err error) {
+	skip, err = strconv.Atoi(chi.URLParam(r, "skip"))
+	if err != nil {
+		err = fmt.Errorf(
+			"%s:%w",
+			err.Error(),
+			&errors.Invalid{
+				errors.Base{
+					"Invalid skip", false,
+				},
+			},
+		)
+		return
+	}
+	return
+}
+
+// queryLimit limits number of datasets per query
+func (list *listHandler) queryLimit(r *http.Request) (
+	limit int, err error,
+) {
+	limit, err = strconv.Atoi(chi.URLParam(r, "limit"))
+
+	if err != nil {
+		err = fmt.Errorf(
+			"%s:%w",
+			err.Error(),
+			&errors.Invalid{
+				errors.Base{
+					"Invalid limit", false,
+				},
+			},
+		)
+		return
+	}
+
+	if limit > 50 {
+		err = &errors.Invalid{
+			errors.Base{
+				"Limit is too big", false,
+			},
+		}
+		return
+	}
+
+	return
+}
+
+func (list *listHandler) askController(
+	listReq dto.ListReq,
+) (
+	resp []dto.ReadResp,
+	err error,
+) {
+	resp, err = list.lister.List(&listReq, listReq.Skip, listReq.Limit)
+	return
+}
+
+func (list *listHandler) handleError(
+	w http.ResponseWriter,
+	err error,
+) {
+	logrus.Error(err)
+	routeutils.ServeError(w, err)
+}
+
+func (list *listHandler) responseSuccess(
+	w http.ResponseWriter,
+	resp []dto.ReadResp,
+) {
+	// Serve a response to the client
+	routeutils.ServeResponse(
+		w,
+		http.StatusOK,
+		resp,
+	)
+}
+
+func (list *listHandler) handleRead(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+
+	var listDat dto.ListReq
+
+	skip, err := list.querySkip(r)
+	limit, err := list.queryLimit(r)
+
+	listDat.Skip = int64(skip)
+	listDat.Limit = int64(limit)
+
+	// Read request from database using skip and limit
+	resp, err := list.askController(listDat)
+
+	if err != nil {
+		list.handleError(w, err)
+		return
+	}
+
+	list.responseSuccess(w, resp)
+}
+
+// ServeHTTP implements http.Handler
+func (list *listHandler) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	defer r.Body.Close()
+
+	list.handleRead(w, r)
+}
+
+// ListRoute provides a route that gives lists for datasets
+func ListRoute(
+	lister parallelsentence.Lister,
+	middleware *middleware.Auth,
+) *routeutils.Route {
+	handler := listHandler{lister}
+	return &routeutils.Route{
+		Method:  http.MethodGet,
+		Pattern: apipattern.ParallelsentenceList,
+		Handler: middleware.Middleware(&handler),
+	}
+}
