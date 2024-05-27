f3bb0e7a52bc0477ece5eb9b96a5d9c11e028967
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-27 06:36:55 +0600

    chore: update comments

--- bootstrap/server/parallelsentence.go
+++ bootstrap/server/parallelsentence.go
@@ -5,7 +5,7 @@ import (
 	"obyoy-backend/parallelsentence"
 )
 
-// Order registers order related providers
+// Order registers parallel sentence related providers
 func Parallelsentence(c container.Container) {
 	c.Register(parallelsentence.NewCreate)
 	c.Register(parallelsentence.NewReader)
