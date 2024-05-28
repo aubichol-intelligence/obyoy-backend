7f97e3d3eceb2de40a570dc9bce30b6782fb74de
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-28 23:28:58 +0600

    fix: make is_translated mandatory for every request

--- store/datastream/mongo/model/datastream.go
+++ store/datastream/mongo/model/datastream.go
@@ -17,7 +17,7 @@ type Datastream struct {
 	SourceLanguage  string             `bson:"source_language,omitempty"`
 	LineNumber      int32              `bson:"line_number,omitempty"`
 	DatasetID       primitive.ObjectID `bson:"dataset_id,omitempty"`
-	IsTranslated    int32              `bson:"is_translated,omitempty"`
+	IsTranslated    int32              `bson:"is_translated"`
 	TimesTranslated int32              `bson:"times_translated,omitempty"`
 	TimesReviewed   int32              `bson:"times_reviewed,omitempty"`
 	CreatedAt       time.Time          `bson:"created_at,omitempty"`
