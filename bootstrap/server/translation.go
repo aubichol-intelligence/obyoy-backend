745c31b0ac20d127f58a7ed102e4f021f4948cd6
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-28 15:35:57 +0600

    chore: update comment

--- bootstrap/server/translation.go
+++ bootstrap/server/translation.go
@@ -5,7 +5,7 @@ import (
 	"obyoy-backend/translation"
 )
 
-// Order registers translation related providers
+// Translation registers translation related providers
 func Translation(c container.Container) {
 	c.Register(translation.NewCreate)
 	c.Register(translation.NewReader)
