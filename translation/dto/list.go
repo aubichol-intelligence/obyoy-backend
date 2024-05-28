bd16b936248eb0645c220732d1174897b86bdf5d
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-28 15:21:24 +0600

    feat: add attribute keys

--- translation/dto/list.go
+++ translation/dto/list.go
@@ -2,6 +2,6 @@ package dto
 
 // ListReq stores translation read request data
 type ListReq struct {
-	UserID  string
-	Contest string
+	UserID        string `json:"user_id"`
+	TranslationID string `json:"translation_id"`
 }
