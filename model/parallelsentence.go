b047ca1817a8818f09c0b8b0ec4aa1e35f7fa4d0
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-29 02:47:08 +0600

    fix: add proper name for the deleted field

--- model/parallelsentence.go
+++ model/parallelsentence.go
@@ -21,7 +21,7 @@ type Parallelsentence struct {
 	ExpiredAt           time.Time `json:"expire_at"`
 	CreatedAt           time.Time `json:"created_at"`
 	UpdatedAt           time.Time `json:"updated_at"`
-	IsDeleted           bool      `json:'false"`
+	IsDeleted           bool      `json:"is_deleted"`
 }
 
 func (s *Parallelsentence) ToByte() ([]byte, error) {
