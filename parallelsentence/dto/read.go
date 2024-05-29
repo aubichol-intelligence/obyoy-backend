d9ad9c01fe2ec3c7d7651f4e6baa8e9e54af7c94
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-05-30 00:32:24 +0600

    feat: add some missing attributes in the read response of parallelsentence

--- parallelsentence/dto/read.go
+++ parallelsentence/dto/read.go
@@ -11,21 +11,28 @@ type ReadReq struct {
 // ReadReq stores parallelsentence read request data
 type ReadResp struct {
 	SourceSentence      string   `json:"source_sentence"`
+	SourceLanguage      string   `json:"source_language"`
 	DestinationSentence string   `json:"destination_sentence"`
+	DestinationLanguage string   `json:"destination_language"`
 	TranslatorID        string   `json:"translator_id"`
 	ReviewedLines       []string `json:"reviewed_lines"`
 	Name                string   `json:"name"`
 	DatasetID           string   `json:"dataset_id"`
+	DatastreamID        string   `json:"datastream_id"`
 	LineNumber          int      `json:"line_number"`
 }
 
 // FromModel converts the model data to response data
 func (r *ReadResp) FromModel(ps *model.Parallelsentence) {
 	r.SourceSentence = ps.SourceSentence
+	r.SourceLanguage = ps.SourceLanguage
 	r.DestinationSentence = ps.DestinationSentence
+	r.DestinationLanguage = ps.DestinationLanguage
 	r.TranslatorID = ps.TranslatorID
 	r.ReviewedLines = ps.ReviewedLines
 	r.Name = ps.DatasetName
 	r.DatasetID = ps.DatasetID
+	r.DatastreamID = ps.DatastreamID
+	r.TranslatorID = ps.TranslatorID
 	r.LineNumber = ps.LineNumber
 }
